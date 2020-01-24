622f74d4a5125299823a5471c384c917386420db
arsen <arsen@LAPTOP-K7E9IAPG>
2020-01-24 21:03:01 +0900

    firstcommit

---
+++ src/helloworld.java
@@ -0,0 +1,9 @@
+
+public class helloworld {
+public static void main(String[] args) {
+	System.out.println("Hello javaworld");
+	System.out.println("Hello javaworld2");
+	System.out.println();
+	System.out.println("HELLO");
+}
+}
